4d6be3e231bfe3b63292ceab0b47a8b5f106550c
donatoway <donatoway@gmail.com>
2021-11-02 23:13:32 +0000

    add

--- CPP-07/ex00/main.cpp
+++ CPP-07/ex00/main.cpp
@@ -1,24 +1,4 @@
 #include "whatever.hpp"
-/*
-Scrivi i seguenti modelli di funzione:
-
-• swap: scambia i valori di due argomenti. Non restituisce nulla.
-
-• min: confronta i due argomenti e restituisce il più piccolo. Se i 
-due argomenti sono uguali, restituisce il secondo.
-
-• max: confronta i due argomenti e restituisce il più grande. Se i
- due argomenti sono uguali, restituisce il secondo.
-
-I modelli devono essere definiti nei file di intestazione. Devi 
-consegnare un main.cpp contenente i test per il tuo programma. 
-Questo file può e verrà modificato durante la valutazione. 
-Queste funzioni possono essere chiamate con qualsiasi tipo di 
-argomento, a condizione che i due argomenti abbiano lo stesso tipo 
-e supportino tutti gli operatori di confronto. Fornire codice 
-sufficiente per compilare un eseguibile che dimostri che tutto 
-funziona come previsto.
-*/
 
 int main()
 {
